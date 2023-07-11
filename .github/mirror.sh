04230622cf4529c87883409b66ed95a410b22175
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2023-07-10 17:09:28 -0700

    actions.

---
+++ .github/mirror.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#!/bin/bash
+
+# Clone the Movement Framework Mirror repository
+git clone https://github.com/movemntdev/movement-framework-mirror.git
+
+# Copy the aptos-move directory to the Movement Framework Mirror repository
+cp -r vm/aptos-vm/aptos-move movement-framework-mirror/
+
+# Change directory to the Movement Framework Mirror repository
+cd movement-framework-mirror/
+
+# Add all changes to Git
+git add -A
+
+# Commit the changes
+git commit -m "Mirror aptos-move directory from current repo"
+
+# Push the changes with force
+git push --force
