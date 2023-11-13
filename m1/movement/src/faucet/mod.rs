034a2e83fcbb8f1d7959b1bf8499009988b074ed
Liam Monninger <l.mak.monninger@gmail.com>
2023-11-13 15:35:46 -0800

    feat: hotfix on #78 and faucet does the same.

--- m1/movement/src/faucet/mod.rs
+++ m1/movement/src/faucet/mod.rs
@@ -12,11 +12,22 @@ use clap::Parser;
 #[derive(Debug, Parser)]
 pub struct FaucetTool {
     #[clap(long)]
-    pub_key: String,
+    pub_key: Option<String>,
     #[clap(flatten)]
     pub(crate) faucet_options: FaucetOptions,
     #[clap(flatten)]
     pub(crate) rest_options: RestOptions,
+    #[clap(flatten)]
+    pub(crate) profile_options: ProfileOptions,
+}
+
+impl FaucetTool {
+    fn pub_key(&self, profile: &ProfileOptions) -> CliTypedResult<String> {
+        match &self.pub_key {
+            Some(pub_key) => Ok(pub_key.clone()),
+            None => Ok((profile.public_key()?).to_string()),
+        }
+    }
 }
 
 #[async_trait]
@@ -26,15 +37,15 @@ impl CliCommand<String> for FaucetTool {
     }
 
     async fn execute(self) -> CliTypedResult<String> {
-        let profile = ProfileOptions::default();
+        let profile = &self.profile_options;
         let hashes = fund_pub_key(
             self.faucet_options.faucet_url(&profile)?,
-            self.pub_key.clone(),
+            self.pub_key(&profile)?,
         ).await?;
         let client = self.rest_options.client_raw(self.faucet_options.faucet_url(&profile)?)?;
         wait_for_transactions(&client, hashes).await?;
         return Ok(format!(
-            "Added 1000_000_000 Octas to account {}", self.pub_key
+            "Added 10 MOV to account {}", self.pub_key(&profile)?
         ));
     }
 }
