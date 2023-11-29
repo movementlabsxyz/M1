af55b56200d7351b2b44bc247970335dd1ba70c0
Liam Monninger <l.mak.monninger@gmail.com>
2023-11-28 16:32:17 -0800

    feat(wip): integrated movementctl.

--- movement-sdk/clis/movement/src/lib.rs
+++ movement-sdk/clis/movement/src/lib.rs
@@ -5,3 +5,13 @@ pub mod ctl;
 pub mod aptos;
 pub mod sui;
 pub mod canonical;
+
+use async_trait::async_trait;
+use clap::Parser;
+use ctl::ctl::Ctl;
+
+#[derive(Parser)]
+#[clap(name = "movement", author, version, propagate_version = true)]
+pub enum Movement {
+
+}
