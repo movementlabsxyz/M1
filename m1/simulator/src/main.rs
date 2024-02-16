9b0a2333262982fefa948f78e68149a7e35775e3
Richard Melkonian <r.v.melkonian@gmail.com>
2024-02-16 16:24:26 +0000

    update network init, refactors

--- m1/simulator/src/main.rs
+++ m1/simulator/src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use simulator::Network;
 
 type NodeId = u64;
 
@@ -10,36 +11,60 @@ pub struct Cli {
     pub command: SubCommands,
 }
 
-/// Start the simulator 
+/// Start the simulator
 #[derive(Debug, Parser)]
 pub struct StartCommand {
     /// The number of validators for the network
-    #[clap(long, default_value = "5", help = "The number of validators for the network.")]
+    #[clap(
+        long,
+        default_value = "5",
+        help = "The number of validators for the network."
+    )]
     pub validators: u64,
 
     /// Sets if the validators join the network at once, or in a staggered way
-    #[clap(long, default_value = "false", help = "Sets if the validators join the network at once, or in a staggered way.")]
+    #[clap(
+        long,
+        default_value = "false",
+        help = "Sets if the validators join the network at once, or in a staggered way."
+    )]
     pub staggered: bool,
 
     /// Verbose output
     #[clap(short, long, help = "Verbose output.")]
-    pub verbose: bool
+    pub verbose: bool,
+
+    /// Run simuilator against the local network
+    #[clap(long, help = "Run simuilator against the local network.")]
+    pub local: bool,
+
+    /// The GRPC endpoint of the network runner to connect to
+    #[clap(long, help = "The GRPC endpoint of the network runner to connect to.")]
+    pub grpc_endpoint: Option<String>,
 }
 
 /// Partition the network
 #[derive(Debug, Parser)]
 pub struct PartitionCommand {
     /// The percentage of validators that will be partitioned
-    #[clap(long, default_value = "5", help = "The percentage of validators that will be in a partitioned state")]
-    pub amount: u8, 
+    #[clap(
+        long,
+        default_value = "5",
+        help = "The percentage of validators that will be in a partitioned state"
+    )]
+    pub amount: u8,
 
     /// Sets if the validators become paritioned at once or in a staggered way
-    #[clap(long, default_value = "false", help = "Sets if the validators become partitioned at once or in a staggered way.")]
+    #[clap(
+        long,
+        default_value = "false",
+        help = "Sets if the validators become partitioned at once or in a staggered way."
+    )]
     pub staggered: bool,
 
     /// Verbose output
     #[clap(short, long, help = "Verbose output.")]
-    pub verbose: bool
+    pub verbose: bool,
 }
 
 #[derive(Debug, Parser)]
@@ -53,21 +78,21 @@ pub struct ReconnectCommand {
 
     /// Verbose output
     #[clap(short, long, help = "Verbose output.")]
-    pub verbose: bool
+    pub verbose: bool,
 }
 
 #[derive(Debug, Parser)]
 pub struct HealthCommand {
     /// Verbose ouput
-     #[clap(short, long, help = "Verbose output.")]
-    pub verbose: bool
+    #[clap(short, long, help = "Verbose output.")]
+    pub verbose: bool,
 }
 
 #[derive(Debug, Subcommand)]
-pub enum SubCommands{
+pub enum SubCommands {
     /// Starts the network with a number of validators
     Start(StartCommand),
-    /// Simulates a network partition. 
+    /// Simulates a network partition.
     Partition(PartitionCommand),
     /// Reconnects the validators after they have become partitioned
     Reconnect(ReconnectCommand),
@@ -76,29 +101,37 @@ pub enum SubCommands{
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), anyhow::Error> {
     let cli = Cli::parse();
     match cli.command {
-        SubCommands::Start(opts) => start_network(opts).await,
-        SubCommands::Partition(opts) => partition_network(opts).await,
-        SubCommands::Reconnect(opts) => reconnect_validators(opts).await,
-        SubCommands::Health(opts) => network_health(opts).await,
+        SubCommands::Start(opts) => start_network(opts).await?,
+        SubCommands::Partition(opts) => partition_network(opts).await?,
+        SubCommands::Reconnect(opts) => reconnect_validators(opts).await?,
+        SubCommands::Health(opts) => network_health(opts).await?,
     }
+    Ok(())
 }
 
-async fn start_network(opts: StartCommand) {
-    simulator::init_m1_network().await;
+async fn start_network(opts: StartCommand) -> Result<(), anyhow::Error> {
+    let net = Network::new(
+        opts.local,
+        opts.grpc_endpoint,
+        opts.staggered,
+        "avalanchego_path".to_string(),
+        "vm_plugin_path".to_string(),
+    )?;
+    net.init_m1_network().await?;
+    Ok(())
 }
 
-async fn partition_network(opts: PartitionCommand) {
-    todo!()
+async fn partition_network(opts: PartitionCommand) -> Result<(), anyhow::Error> {
+    Ok(())
 }
 
-async fn reconnect_validators(opts: ReconnectCommand) {
-    todo!()
-
+async fn reconnect_validators(opts: ReconnectCommand) -> Result<(), anyhow::Error> {
+    Ok(())
 }
 
-async fn network_health(opts: HealthCommand) {
-    todo!()
+async fn network_health(opts: HealthCommand) -> Result<(), anyhow::Error> {
+    Ok(())
 }
